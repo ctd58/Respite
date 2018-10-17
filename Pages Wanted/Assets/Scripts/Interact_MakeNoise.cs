af901ad6f6dfd13f7c001ce39a1183114731e86a
LilyLauben <lml337@drexel.edu>
2018-10-17 12:00:25 -0400

    Finished handbell, started setup for clock

---
+++ Pages Wanted/Assets/Scripts/Interact_MakeNoise.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interact_MakeNoise : Interactables {
+
+	private AudioSource audioClip;
+	private Sound soundScript;
+	[Range(0, 5)]
+	public float soundVolume;
+
+	void Start()
+	{
+		audioClip = this.GetComponent<AudioSource>();
+		soundScript = this.GetComponent<Sound>();
+	}
+
+	
+	public override void onInteract(Character_Inventory inv) {
+		
+	}
+
+	protected void MakeNoise() {
+		soundScript.sound = soundVolume;
+		audioClip.Play();
+	}
+
+	
+}
---
+++ Pages Wanted/Assets/Scripts/Interact_MakeNoiseNow.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interact_MakeNoiseNow : Interact_MakeNoise {
+	
+	public override void onInteract(Character_Inventory inv) {
+		PlayAnimation();
+		MakeNoise();
+	}
+
+	public void PlayAnimation() {
+		//TODO: stuff here
+	}
+}
