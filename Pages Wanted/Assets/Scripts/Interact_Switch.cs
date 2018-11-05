ef4a5dc037e5f4026e5f4688812395ce78f863f2
LilyLauben <lml337@drexel.edu>
2018-11-04 21:22:20 -0500

    created lever scripts

---
+++ Pages Wanted/Assets/Scripts/Interact_Switch.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interact_Switch : Interactables {
+
+	// Public or Serialized Variables for Inspector -----------------
+	#region Public Variables
+	public List<Switch_Target> targets;
+	#endregion
+
+	// Private Variables ---------------------------------------------
+	#region Private Variables
+	bool isActivated = false;
+	#endregion
+
+	void Start() {
+		SetSprite(Interact_Icon_Type.PICKUP);
+	}
+
+	public override void onInteract(Character_Inventory inv) {
+		foreach (Switch_Target target in targets) {
+			if (!isActivated) target.onSwitchActivate();
+			else target.onSwitchDeactivate();
+		}
+		isActivated = !isActivated;
+	}
+}
---
+++ Pages Wanted/Assets/Scripts/Switch_Target.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch_Target : MonoBehaviour {
+
+	private Animator animator;
+
+	void Start() {
+		animator = GetComponent<Animator>();
+	}
+
+	public void onSwitchActivate () {
+		animator.SetTrigger("open");
+	}
+
+	public void onSwitchDeactivate() {
+		animator.SetTrigger("close");
+	}
+}
