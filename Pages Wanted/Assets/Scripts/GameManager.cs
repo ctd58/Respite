929aaccfe37903a578d19818c84a830f6889789b
binaryPersona <sorafnlform@gmail.com>
2018-11-15 13:47:12 -0500

    Add files via upload

---
+++ Pages Wanted/Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager instance = null;
+
+    private AudioSource audio;
+
+    private void Awake() {
+        if (instance == null) {
+            instance = this;
+        } else if (instance != this) {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+    // Use this for initialization
+    void Start () {
+        audio = GetComponent<AudioSource>();
+        audio.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
