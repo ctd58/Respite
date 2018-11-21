95c43384a4cfd5f9657e24a711d0d17b200d690d
Widchard <wf77@drexel.edu>
2018-11-21 17:39:34 -0500

    Started Cleaning Up Incantations

---
+++ Pages Wanted/Assets/Scripts/Incantations.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Incantations : MonoBehaviour {
+
+    //Public variables
+    
+
+    //Private variables
+    private GameObject player;
+    private MonsterManager mm;
+    private Sound pSound; 
+
+
+    //Public methods
+
+    
+    //Private methods
+
+
+    //Possible coroutines
+}
