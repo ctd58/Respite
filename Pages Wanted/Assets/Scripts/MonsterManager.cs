e8cbe10716277c9f788f622636ba9063c851a23c
Zoe Wolfe <sorafnlform@gmail.com>
2018-11-21 22:01:35 -0500

    All prefabs are here, worked on resizing.  The basement should be done.  Monster speed **may** need to be adjusted.

--- Pages Wanted/Assets/Scripts/MonsterManager.cs
+++ Pages Wanted/Assets/Scripts/MonsterManager.cs
@@ -79,11 +79,6 @@ public class MonsterManager : MonoBehaviour {
         currentState = STATE.HUNT;
         StartCoroutine("HuntPlayers");
     }
-
-    private void EnterStateIdle() {
-        currentState = STATE.IDLE;
-        
-    }
     #endregion
 
     // Set New Target -------------------------------------------------
@@ -222,9 +217,6 @@ public class MonsterManager : MonoBehaviour {
     }
     #endregion
 
-    // Slows the monster down or speeds it up smoothly
-    // newSpeed = the new speed we want the monster to be moving
-    // seconds  = the number of seconds the monster will take to reach that new speed
     public IEnumerator ChangeSpeed(float newSpeed, float seconds) {
         float speedIncrement = (currentSpeed-newSpeed)/(seconds/0.1f);
         for (float i = 0; i < seconds; i += 0.1f) {
@@ -234,8 +226,6 @@ public class MonsterManager : MonoBehaviour {
         }
     }
 
-    // Reverts the monster to its base speed smoothly
-    // seconds  = the number of seconds the monster will take to reach baseSpeed
     public IEnumerator ReturnToBaseSpeed(float seconds) {
         float speedIncrement = (currentSpeed-baseMonsterSpeed)/(seconds/0.1f);
         for (float i = 0; i < seconds; i += 0.1f) {
