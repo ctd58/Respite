6b811862f8209156759b6b8648d6c7a8f03d6196
Widchard <wf77@drexel.edu>
2018-11-28 20:50:19 -0500

    Journal Changes

--- Pages Wanted/Assets/Scripts/Journal/Journal.cs
+++ Pages Wanted/Assets/Scripts/Journal/Journal.cs
@@ -4,13 +4,49 @@ using UnityEngine;
 
 public class Journal : MonoBehaviour {
 
+    private string[] dialogue;
+    private string[] objectives;
+
+    private static bool journalon; 
+    private string button;
+    public GameObject canvas; 
+    private GameObject journal;
+    private GameObject player;
+    private JournalManager jm; 
+
+
+
 	// Use this for initialization
 	void Start () {
-		
-	}
+		if (player.tag == "P1") {
+            button = "P1lbrb";
+            //journal = canvas.GetComponent<P1_Screen>().GetComponent<Journal>(); 
+        }
+        if (player.tag == "P2") {
+            button = "P2lbrb";
+            //journal = canvas.GetComponent<P2_Screen>().GetComponent<Journal>();
+        }
+        dialogue = jm.GetDialogue();
+        objectives = jm.GetObjective(); 
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.GetButtonDown(button)) {
+            ToggleJournal(); 
+        }
 	}
+
+    void ToggleJournal() {
+        if (journalon) {
+            journalon = false;
+            journal.SetActive(false); 
+        }
+        else {
+            journalon = true;
+            journal.SetActive(true); 
+        }
+    }
+    
+
 }
