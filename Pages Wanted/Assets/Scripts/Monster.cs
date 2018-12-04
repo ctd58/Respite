dd5c39a0342c167097590f88accaf15eb59e27ce
LilyLauben <lml337@drexel.edu>
2018-12-03 21:51:41 -0500

    Added color and speed changes to monstermanager depending on state

--- Pages Wanted/Assets/Scripts/Monster.cs
+++ Pages Wanted/Assets/Scripts/Monster.cs
@@ -10,7 +10,8 @@ public class Monster : MonoBehaviour {
     [SerializeField] [Range(3.0F, 10.0F)] private float stunDelay = 5.0f;
     public float slowSpeed = 100f;
     public Image insignia1;
-    public Image insignia2; 
+    public Image insignia2;
+    public ParticleSystem stateParticles;
 
     // Private Variables ----------------------------------------------
     private NavMeshAgent navMeshAgent;  
@@ -26,14 +27,6 @@ public class Monster : MonoBehaviour {
     {
         insignia1.enabled = false;
         insignia2.enabled = false;
-        /*
-        GameObject other = GameObject.Find("TitleScreenNav");
-        TitleScreenNav titleScreenNav = other.GetComponent<TitleScreenNav>();
-        if (titleScreenNav != null) {
-            baseMoveSpeed = titleScreenNav.monsterBaseSpeed.value;
-            sensePlayerDistance = titleScreenNav.monsterSense.value;
-        }
-        */
         if (debug) Debug.Log(PlayerPrefs.GetFloat("monsterbasespeed") + "  " + PlayerPrefs.GetFloat("monstersense"));
         checkprefs();
         navMeshAgent = this.GetComponent<NavMeshAgent>();
@@ -47,30 +40,10 @@ public class Monster : MonoBehaviour {
         {
             PlayerPrefs.SetFloat("monsterspeed", 500f);
         }
-        /*
-        if (PlayerPrefs.GetFloat("monstersense") == 0.0f || PlayerPrefs.GetFloat("monstersense") < 300f || PlayerPrefs.GetFloat("monstersense") > 800f)
-        {
-            PlayerPrefs.SetFloat("monstersense", 600f);
-        }
-        */
     }
     
     void Update () {
-        // Debug.Log(_currentState);
-        // switch (_currentState) {
-		// case STATE.WANDER:
-		// 	UpdateWander ();
-		// 	break;
-		// case STATE.INSPECT:
-		// 	UpdateInspect ();
-		// 	break;
-		// case STATE.ATTACK:
-		// 	UpdateAttack ();
-		// 	break;
-		// case STATE.STUNNED:
-		// 	UpdateStun();
-		// 	break;
-		// }
+        //TODO: move insignia to UIManager
         if (insignia1.enabled == true || insignia2.enabled == true) {
             imgnum++; 
             if (imgnum == imgCounter) {
@@ -84,154 +57,6 @@ public class Monster : MonoBehaviour {
         CheckHitPlayer();
 	}
 
-    // WANDER STATE ---------------------------------------------------------------------
-
-    // // code to setup wandering
-    // public void TriggerWander(List<Transform> newWaypoints) {
-    //     wayPoints = newWaypoints;
-    //     i = 0;
-    //     EnterStateWander();
-    // }
-    
-    // private void EnterStateWander() {
-    //     currentMovSpeed = baseMoveSpeed; 
-	// 	_currentState = STATE.WANDER;
-    //     currentWaypoint = wayPoints[i];
-    //     madeWaypoint = true; 
-	// }
-
-	// private void UpdateWander() {
-    //     counter++;
-    //     //Debug.Log(counter); 
-    //     if (counter > 400) {
-    //         counter = 0;
-    //        // Debug.Log("Made waypoint");
-    //         madeWaypoint = true; 
-    //     } else {
-    //         if (currentWaypoint != null && madeWaypoint == true) {
-    //             Vector3 targetV = currentWaypoint.transform.position;
-    //             target = currentWaypoint.transform;
-    //             //Need to make this based on speed, so the idea is maybe a while loop
-    //             //where we tell the demon to move to a certain distance with base speed, and
-    //             //if it no longer needs to move there because it is there, set the next waypoint.
-    //             //navMeshAgent.speed = 1000;
-    //             navMeshAgent.SetDestination(targetV);
-    //             madeWaypoint = false;
-    //             i = (i + 1) % wayPoints.Count;
-    //          //   Debug.Log(i); 
-    //         }
-    //         currentWaypoint = wayPoints[i]; 
-    //     }
-    //     if (monstermanager.DetectPlayer()) { EnterStateAttack(); }
-	// }
-
-
-
-    // INSPECT STATE ---------------------------------------------------------------------
-    // public void CreateNewWaypoint(){
-    //     //Update function to do smart pathing. 
-    //     if (target != null){
-    //         wayPoints.Add(target);
-    //     }
-    // }
-
-    // public void DeleteNewWaypoint(Transform badwaypoint){
-    //     if (wayPoints.Contains(badwaypoint)){
-    //         //tell that waypoint to die
-    //         wayPoints.Remove(badwaypoint);
-    //     }
-    // }
-
-    // public void TriggerInspect() {
-    //     EnterStateInspect();
-    // }
-
-    // private void EnterStateInspect() {
-	// 	_currentState = STATE.INSPECT;
-	// }
-
-	// private void UpdateInspect() {
-    //     //Debug.Log(target.name);
-    //     //Increases speed if it has been chasing for a multiple of 2.5 seconds
-    //     // chaseTime += Time.deltaTime;
-    //     // if (chaseTime / 2.5f > 1.0f) {
-    //     //     chaseTime -= 2.5f;
-    //     //     currentMovSpeed += 7.5f;
-    //     //     if(currentMovSpeed> 800f) {
-    //     //         currentMovSpeed = 800f;
-    //     //     }
-    //     // }
-    //     target = monstermanager.getTarget(); 
-    //     LookAt(target);
-    //     Vector3 targetV = target.position;
-    //     transform.LookAt(target); 
-    //     navMeshAgent.SetDestination(targetV);
-    //     //Check if it is on top of the targets position
-    //     if(Vector3.Distance(target.position,this.transform.position) < senseDistance) {
-    //         Debug.Log("HERE");
-    //         EnterStateWander();
-    //     }
-    //     if (monstermanager.DetectPlayer()) { EnterStateAttack(); }
-	// }
-
-    // ATTACK STATE ---------------------------------------------------------------------
-
-    // public void TriggerAttack() {
-    //     EnterStateAttack(); 
-    // }
-
-
-	// private void EnterStateAttack() {
-	// 	_currentState = STATE.ATTACK;
-    //     UpdateAttack(); 
-	// }
-
-	// private void UpdateAttack() {
-    //     //Debug.Log("Attack!"); 
-    //     if (monstermanager.DetectPlayer() == true) {
-    //         target = monstermanager.getTarget();
-    //         Vector3 targetV = target.position; 
-    //         navMeshAgent.SetDestination(targetV);
-    //         //UpdateAttack(); 
-    //         //EnterStateWander(); 
-    //     } else {
-    //         EnterStateWander(); 
-    //     }
-    // }
-
-    // STUN STATE ---------------------------------------------------------------------
-
-    // TODO: figure out how we want to treat Stun
-    // Player ability script should call this function
-	// public void Stun() {
-	// 	EnterStateStun ();
-	// }
-
-	// private void EnterStateStun() {
-	// 	_currentState = STATE.STUNNED;
-	// 	StartCoroutine("stun");
-	// }
-
-	// private void UpdateStun() {
-	// } // do nothing here, the stun coroutine will set state back to wander
-
-    // IEnumerator stun()
-    // {
-    //     yield return new WaitForSecondsRealtime(stunDelay);
-    //     EnterStateWander();
-    // }
-
-
-
-
-
-
-
-
-
-
-
-    // NEW STUFF
     public void LookAt(Vector3 t) {
         transform.LookAt(t);
         //TODO: make this animate a bit
@@ -269,6 +94,7 @@ public class Monster : MonoBehaviour {
 
 // If player effect player health and then respawn
     void CheckHitPlayer() {
+        //TODO: fix this to not depend on player being the target
         if (Vector3.Distance(transform.position, target.position) < collideDistance) {
             if(target.gameObject.tag == "P1" || target.gameObject.tag == "P2") {
                 if (debug) Debug.Log("Hit Player!");
@@ -287,5 +113,10 @@ public class Monster : MonoBehaviour {
     void respawn() {
         navMeshAgent.Warp(monstermanager.GetSpawnPoint().position);
         //EnterStateStun();
+    }
+
+    public void ChangeParticleColor(Color newColor) {
+        var col = stateParticles.main.startColor;
+        col.color = newColor;
     }
 }
