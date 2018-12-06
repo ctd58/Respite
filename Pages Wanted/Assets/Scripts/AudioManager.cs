9849daa638816be653ee0ed5e974dbb98397e15b
Chris Deslongchamp <ctd58@drexel.edu>
2018-12-05 21:35:45 -0500

    Adding Audio Manager and Dialogue Manager

---
+++ Pages Wanted/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+
+    public AudioSource audio;
+    public GameObject[] dBoxes;
+    public List<AudioClip> que = new List<AudioClip>();
+
+
+    // Use this for initialization
+    void Start () {
+        audio = this.GetComponent<AudioSource>();
+        dBoxes = GameObject.FindGameObjectsWithTag("Dialogue");
+        audio.Play();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(que != null && !dialoguePlaying())
+        {
+            audio.clip = que[0];
+            que.RemoveAt(0);
+            audio.Play();
+        }
+    }
+
+    public bool dialoguePlaying()
+    {
+        //Checks to see if any dialogue boxes are playing sound
+       foreach (GameObject d in dBoxes)
+        {
+            if(d.GetComponent<AudioSource>().isPlaying)
+            {
+                return true;
+            }
+        }
+       //Checks if the intro clip is playing
+       if(audio.isPlaying)
+        {
+            return true;
+        }
+       return false;
+    }
+}
---
+++ Pages Wanted/Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+    public AudioSource audio;
+    public AudioClip Lidia;
+    public AudioClip Aleksy;
+    public string collidingPlayer;
+    public bool dontPlay = false;
+    
+    public AudioManager am;
+
+    // Use this for initialization
+    void Start()
+    {
+        audio = this.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "P1" && !dontPlay && !am.dialoguePlaying())
+        {
+            dontPlay = true;
+            collidingPlayer = "P1";
+            audio.clip = Aleksy;
+            audio.Play();
+        }
+        else if(other.gameObject.tag == "P1" && !dontPlay && am.dialoguePlaying())
+        {
+            dontPlay = true;
+            collidingPlayer = "P1";
+            am.que.Add(Aleksy);
+        }
+        else if (other.gameObject.tag == "P2" && !dontPlay && !am.dialoguePlaying())
+        {
+            dontPlay = true;
+            collidingPlayer = "P2";
+            audio.clip = Lidia;
+            audio.Play();
+        }
+        else if (other.gameObject.tag == "P2" && !dontPlay && am.dialoguePlaying())
+        {
+            dontPlay = true;
+            collidingPlayer = "P1";
+            am.que.Add(Lidia);
+        }
+
+    }
+}
