4d3a1a45f87fb284967eaabc1f957c83109e5da0
LilyLauben <lml337@drexel.edu>
2018-12-12 23:33:34 -0500

    set up final monster states and scene layout

---
+++ Pages Wanted/Assets/Win.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Win : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.tag =="P1" || other.gameObject.tag =="P1") {
+			SceneManager.LoadScene("WinScreen");
+		}
+	}
+}
